a0561cac8ee78927ae2f60ba868c1254e33f0473
Ywatch615 <sundram.22460@knit.ac.in>
2025-04-06 01:53:56 +0530

    create another file named sundu.jsx, can't name it sundu.js cuz the vite react doesn't support in this project, it renders .jsx format, and then int App.jsx we import that Sundu.jsx(keep in mind that while importing write the component's name starting with capital letter) and in the return  statement, if you tryna add some other tags it will show error to overcome this, need to add  a div tag and write all the tags in it or you can just write <> empty angular brackets and ad them

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Sundu from './sundu'
 
 function App() {
 
 
   return (
-    <h1>React and Vite with Sundu</h1>
+    <>
+      <h1>hello world!</h1>
+      <Sundu />
+      <p>hey</p>
+    </>
   )
 }
 
---
+++ 01vitereact/src/sundu.jsx
@@ -0,0 +1,7 @@
+function Sundu(){
+    return (
+        <h2>hello there!</h2>
+    )
+}
+
+export default Sundu
