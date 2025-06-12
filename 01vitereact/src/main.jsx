870cc847615ec6aa5b61bc4afa7d60ad061af8f5
Ywatch615 <sundram.22460@knit.ac.in>
2025-06-12 10:00:22 +0530

    saw how does the create reactElement basically works and its implemetation

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -27,3 +27,27 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     // anotherEle
     // reactElement // React.createElement example
 )
+
+
+/*
+this is how React.createElement works:
+
+function ReactElement(type,key,ref,self,source,owner,props){
+    const element = {
+        $$typeof: REACT_ELEMENT_TYPE,
+
+        Built-in properties that belong on the element
+
+        type,
+        key,
+        ref,
+        props,
+
+
+        Record the component responsible for creating this element.
+        _owner: owner
+        
+    };
+
+    and many more
+*/ 
