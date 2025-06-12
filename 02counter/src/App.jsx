d3e3cb0e1cf9471cde4e99bb5f49f79a97aa57e9
Ywatch615 <sundram.22460@knit.ac.in>
2025-06-12 12:45:25 +0530

    done the assignment

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,20 +5,21 @@ import './App.css'
 
 function App() {
 
-  let [counter,setCounter] = useState(15)// here counter is a variable as declared below using let  and setCounter is the function responsible for the updation of this variable counter and 15 is the default value that we gave to it
+  let [counter,setCounter] = useState(10)// here counter is a variable as declared below using let  and setCounter is the function responsible for the updation of this variable counter and 15 is the default value that we gave to it
   // it is not necessary to give the nomenclature as setCounter, we can name it anything as it is a function we just named it for our understanding 
   // let counter = 15;
 
   const addValue=()=>{
     // counter=counter +1
     setCounter(counter+1)// instead we can pass above line as setCounter(counter) when we'll uncomment the above line both the things will work same way
-    console.log("Counter value is: ", counter);
+    // console.log("Counter value is: ", counter);
+    if(counter >=20) setCounter(20) // this will not allow the counter to go above 20
   }
 
   const removeValue=()=>{
     setCounter(counter-1)
-    console.log("Counter value is: ", counter);
-    
+    // console.log("Counter value is: ", counter);
+    if(counter <=0) setCounter(0) // this will not allow the counter to go below 0
   }
 
   return (
