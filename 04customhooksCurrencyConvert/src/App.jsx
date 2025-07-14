556ffcd51b9f0976e4c64cdb660facb4c761d3e5
Ywatch615 <sundram.22460@knit.ac.in>
2025-07-15 00:26:15 +0530

    this is the way to define a custom hook, remember the hook file will  be .js and not .jsx

---
+++ 04customhooksCurrencyConvert/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* <h1 className='px-3 py-2 mx-4 my-3 text-white'>hii</h1> */}
+      <h1 className='text-3xl bg-orange-400'>Currency Converter</h1>
+    </>
+  )
+}
+
+export default App
