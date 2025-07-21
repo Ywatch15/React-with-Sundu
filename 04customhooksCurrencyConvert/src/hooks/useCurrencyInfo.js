12c43e5c4a1042fe17ffe3fb78f8e4166d802dfc
Ywatch615 <sundram.22460@knit.ac.in>
2025-07-21 20:46:11 +0530

    make the currency convert project by fetching an open-source API and learned the cutom Hooks

--- 04customhooksCurrencyConvert/src/hooks/useCurrencyInfo.js
+++ 04customhooksCurrencyConvert/src/hooks/useCurrencyInfo.js
@@ -1,16 +1,15 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState} from "react"
+
 
 function useCurrencyInfo(currency){
-    const [data, setData] = useState({});
-    useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/{currency}.json
-`)
-.then((response)=>response.json())
-.then((response)=> setData(response[currency]))
-console.log(data);
-
-    },[currency])
-    console.log(data);
-    return data;
-    
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        .catch(err => console.log("Error fetching currency data:", err));
+    }, [currency])
+    return data
 }
+
+export default useCurrencyInfo;
